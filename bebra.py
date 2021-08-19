a4d833ee34733731735ba280e8702831e7944905
nikoff11 <89134277+nikoff11@users.noreply.github.com>
2021-08-19 10:10:49 +0700

    Add files via upload

---
+++ bebra.py
@@ -0,0 +1,27 @@
+# a = int(input("chislo"))
+# b = int(input("chislo"))
+# s = input("znak")
+# if s == "+" :
+#     c = a + b
+#     print(c)
+# elif s == "*" :
+#     c = a * b
+#     print(c)
+# elif s == "/" :
+#     c = a / b
+#     print(c)
+# else :
+#     c = a - b
+#     print(c)
+str=input("6 chisel")
+a = int(str[0])
+b = int(str[1])
+c = int(str[2])
+d = int(str[3])
+e = int(str[4])
+r = int(str[5])
+if (a + b + c)  == (d + e + r) :
+    print("schastliviy")
+else :
+    print("obichniy")
+
