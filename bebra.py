f2a1c0a88ba303004fe04287158d13988e03acc5
nikoff11 <89134277+nikoff11@users.noreply.github.com>
2021-08-19 12:38:18 +0700

    Add files via upload

--- bebra.py
+++ bebra.py
@@ -1,27 +1,79 @@
-# a = int(input("chislo"))
-# b = int(input("chislo"))
-# s = input("znak")
-# if s == "+" :
-#     c = a + b
-#     print(c)
-# elif s == "*" :
-#     c = a * b
-#     print(c)
-# elif s == "/" :
-#     c = a / b
-#     print(c)
-# else :
-#     c = a - b
-#     print(c)
-str=input("6 chisel")
-a = int(str[0])
-b = int(str[1])
-c = int(str[2])
-d = int(str[3])
-e = int(str[4])
-r = int(str[5])
-if (a + b + c)  == (d + e + r) :
-    print("schastliviy")
-else :
-    print("obichniy")
-
+# # a = int(input("chislo"))
+# # b = int(input("chislo"))
+# # s = input("znak")
+# # if s == "+" :
+# #     c = a + b
+# #     print(c)
+# # elif s == "*" :
+# #     c = a * b
+# #     print(c)
+# # elif s == "/" :
+# #     c = a / b
+# #     print(c)
+# # else :
+# #     c = a - b
+# #     print(c)
+# # str=input("6 chisel")
+# # a = int(str[0])
+# # b = int(str[1])
+# # c = int(str[2])
+# # d = int(str[3])
+# # e = int(str[4])
+# # r = int(str[5])
+# # if (a + b + c)  == (d + e + r) :
+# #     print("schastliviy")
+# # else :
+# #     print("obichniy")
+# # s = int(input("chislo\n"))
+# # b = 0
+# # for i in range(s) :
+# #     a = int(input("chislo\n"))
+# #     b = a + b
+# # print(b)
+# # a, b, c = map (int,input().split())
+# # str=input("chisla")
+# # while str(i) != 0 :
+# #     i + 1
+# # print(i)
+#
+#
+#
+#
+#
+#
+# # a = int(input("chislo"))
+# # b = int(input("chislo"))
+# # s = input("znak")
+# # if s == "+" :
+# #     c = a + b
+# #     print(c)
+# # elif s == "*" :
+# #     c = a * b
+# #     print(c)
+# # elif s == "/" :
+# #     c = a / b
+# #     print(c)
+# # else :
+# #     c = a - b
+# #     print(c)
+# # str=input("6 chisel")
+# # a = int(str[0]
+# # b = int(str[1])
+# # c = int(str[2])
+# # d = int(str[3])
+# # e = int(str[4])
+# # r = int(str[5])
+# # if (a + b + c)  == (d + e + r) :
+# #     print("schastliviy")
+# # else :
+# #     print("obichniy")
+# # s = int(input("chislo\n"))
+# # b = 0
+# # for i in range(s) :
+# # #     a = int(input("chislo\n"))
+# fn = open("hello.txt", "w")
+# fn.write("hi")
+# fn.close()
+# fn = open("hello.txt", "r")
+# print(fn.read())
+# fn.close()
